4341eef880ffba1cb397d0a4fd58263b53de7c2b
Vashu <vashusuman3105@gmail.com>
2025-07-09 15:37:14 +0530

    It will draw ellipse and rectangle on the tracked video frames

---
+++ drawers/player_tracks_drawer.py
@@ -0,0 +1,37 @@
+from .utils import draw_ellipse
+
+
+class PlayerTracksDrawer:
+    
+    def __init__(self):
+        pass
+
+    def draw(self, video_frames, tracks):
+        """
+        Draws the player tracks on the video frames.
+
+        Args:
+            video_frames (list): List of video frames to draw on.
+            tracks (list): List of dictionaries containing player tracking information for each frame,
+                where each dictionary maps player IDs to their bounding box coordinates.
+
+        Returns:
+            list: List of video frames with drawn player tracks.
+        """
+        output_frames = []
+
+        for frame_num, frame in enumerate(video_frames):
+            frame = frame.copy()
+
+            player_dict= tracks[frame_num]
+
+            # Draw the player tracks on the frame
+
+            for track_id, player in player_dict.items():
+
+                frame = draw_ellipse( frame, player['box'], color = (0,0,255), track_id=track_id)
+
+            output_frames.append(frame)
+
+
+        return output_frames
---
+++ drawers/utils.py
@@ -0,0 +1,70 @@
+import cv2
+import sys
+sys.path.append("../")  
+
+from utils import get_centre_of_bbox, get_bbox_width
+
+def draw_ellipse(frame, bbox, color, track_id=None):
+    """
+    Draws an ellipse on the frame based on the bounding box coordinates.
+
+    Args:
+        frame (numpy.ndarray): The video frame to draw on.
+        bbox (list): The bounding box coordinates [x1, y1, x2, y2].
+        color (tuple): The color of the ellipse in BGR format.
+        track_id (int, optional): The ID of the track. If provided, it will be drawn on the frame.
+
+    Returns:
+        numpy.ndarray: The frame with the drawn ellipse.
+    """
+    x1, y1, x2, y2 = bbox
+
+    x_center,_= get_centre_of_bbox(bbox)
+    width = get_bbox_width(bbox)
+    center = (int(x_center),int( y2))
+    axes = (int(width/2), int(0.35*width))
+    angle = 0.0
+    startAngle = -45.0
+    endAngle = 235.0
+    thickness = 2
+    cv2.ellipse(frame, 
+                center,
+                axes ,
+                angle,
+                startAngle,
+                endAngle,
+                color,
+                thickness)
+                # lineType=cv2.LINE_4)
+    
+    rectangle_width =40
+    rectangle_height = 20
+    x1_rect = x_center -rectangle_width//2
+    x2_rect = x_center + rectangle_width//2
+    y1_rect= (y2 - rectangle_height//2) + 15
+    y2_rect = (y2 + rectangle_height//2) + 15
+
+    if track_id is not None:
+        cv2.rectangle(
+            frame,
+            (int(x1_rect),int(y1_rect)),
+            (int(x2_rect),int(y2_rect)),
+            color,
+            cv2.FILLED
+        )
+
+        x1_text = x1_rect + 12
+        if track_id >99: #if the track id is more than 99, then we need to adjust the x1_text
+            x1_text -= 10
+
+        cv2.putText(
+            frame,
+            str(track_id),
+            (int(x1_text), int(y2_rect+ 15)),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            fontScale=0.6,
+            color=(0,0,0),
+            thickness=2
+        )
+
+    return frame
